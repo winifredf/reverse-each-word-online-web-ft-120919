161cdce22d820bc0f3629b035f0a17f0c7470e31
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 01:21:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,8 @@
 def reverse_each_word(String)
-  
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
