c57662c494e26d43c1a71f2a44a835873a85f5a4
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 01:30:04 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+def reverse_each_word(greeting)
+  original_array = greeting.split(" ")
+  return_array = []
+  original_array.each do|greeting|
+    return_array << greeting.reverse
+  end
+  return_array.join(" ")
+end
+
+reverse_each_word("Hello there, and how are you?") 
+
+def reverse_each_word(greeting)
+  original_array = greeting.split(" ")
+  return_array = []
+  original_array.collect do|greeting|
+    return_array << greeting.reverse
+  end
+  return_array.join(" ")
+end
+
+reverse_each_word("Hello there, and how are you?") 
