05334ab44b9d460aa1350228366478e561759e0e
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 01:24:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(greeting)
   original_array = string.split(" ")
   return_array = []
   original_array.each do|string|
-    return_array << string.reverse
+    return_array << greeting.reverse
   end
   return_array.join(" ")
 end
